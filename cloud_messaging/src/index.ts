7c50172f1e12084660c0b4c3150990996dcdc29f
kakeru-ikeda <kakerumail1451@gmail.com>
2024-10-22 00:25:16 +0900

    feat: Express.js導入

--- cloud_messaging/src/index.ts
+++ cloud_messaging/src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello, world!');
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware
+app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+    next();
+});
+
+app.get('/', (_, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
