97ff5575c0f13602a56e9649cb0549d9b337c8f9
kakeru-ikeda <kakerumail1451@gmail.com>
2024-10-24 02:02:24 +0900

    feat: multicastとtopicによる通知配信機構の追加

--- cloud_messaging/src/module/send_message.ts
+++ cloud_messaging/src/module/send_message.ts
@@ -3,25 +3,60 @@ import { CloudMessagingService } from './service/firebase/cloud_messaging_servic
 export class SendMessage {
     private req: Request;
     private res: Response;
+    private cloudMessagingService: CloudMessagingService;
+    private title: string;
+    private body: string;
 
     constructor(req: Request, res: Response) {
         this.req = req;
         this.res = res;
+        this.cloudMessagingService = new CloudMessagingService();
+        this.title = this.req.body.title;
+        this.body = this.req.body.body;
+
+        console.log('Title:', this.title);
+        console.log('Body:', this.body);
     }
 
     /**
      * Send a message to a device using the Firebase Cloud Messaging token.
      */
     async useToken() {
-        const { title, body } = this.req.body;
         const token = this.req.headers['x-firebase-cloud-messaging-token'] as string;
-        console.log('Title:', title);
-        console.log('Body:', body);
         console.log('Token:', token);
 
         try {
-            const cloudMessagingService = new CloudMessagingService();
-            const res = await cloudMessagingService.sendMessage(title, body, token);
+            const res = await this.cloudMessagingService.send(this.title, this.body, token);
+            this.res.send(res);
+        } catch (error) {
+            this.res.status(500).send(error);
+        }
+    }
+
+    /**
+     * Send a message to multiple devices using the Firebase Cloud Messaging tokens.
+     */
+    async useMulticast() {
+        const tokens = this.req.body.tokens as string[];
+        console.log('Tokens:', tokens);
+
+        try {
+            const res = await this.cloudMessagingService.multicast(this.title, this.body, tokens);
+            this.res.send(res);
+        } catch (error) {
+            this.res.status(500).send(error);
+        }
+    }
+
+    /**
+     * Send a message to a topic using the Firebase Cloud Messaging topic.
+     */
+    async useTopic() {
+        const topic = this.req.body.topic as string;
+        console.log('Topic:', topic);
+
+        try {
+            const res = await this.cloudMessagingService.sendToTopic(this.title, this.body, topic);
             this.res.send(res);
         } catch (error) {
             this.res.status(500).send(error);
--- cloud_messaging/src/module/service/firebase/cloud_messaging_service.ts
+++ cloud_messaging/src/module/service/firebase/cloud_messaging_service.ts
@@ -2,13 +2,13 @@ import { getMessaging } from "firebase-admin/messaging";
 import { FirebaseCore } from "../core/firebase_core";
 
 export class CloudMessagingService extends FirebaseCore {
-    async sendMessage(title: string, body: string, token: string) {
+    async send(title: string, body: string, token: string) {
         const message = {
             notification: {
                 title,
                 body,
             },
-            token,
+            token: token,
         };
 
         console.log('Sending message...');
@@ -19,6 +19,48 @@ export class CloudMessagingService extends FirebaseCore {
             return res;
         } catch (error) {
             console.error('Error sending message:', error);
+            throw error;
+        }
+    }
+
+    async multicast(title: string, body: string, tokens: string[]) {
+        const message = {
+            notification: {
+                title,
+                body,
+            },
+            tokens: tokens,
+        };
+
+        console.log('Sending multicast message...');
+
+        try {
+            const res = await getMessaging().sendEachForMulticast(message);
+            console.log('Successfully sent multicast message:', res);
+            return res;
+        } catch (error) {
+            console.error('Error sending multicast message:', error);
+            throw error;
+        }
+    }
+
+    async sendToTopic(title: string, body: string, topic: string) {
+        const message = {
+            data: {
+                title,
+                body,
+            },
+            topic: topic,
+        };
+
+        console.log('Sending message to topic...');
+
+        try {
+            const res = await getMessaging().send(message);
+            console.log('Successfully sent message to topic:', res);
+            return res;
+        } catch (error) {
+            console.error('Error sending message to topic:', error);
             throw error;
         }
     }
