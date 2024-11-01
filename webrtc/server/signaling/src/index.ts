b711bd5171367f388492aad2f568b35f7613cead
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-01 15:44:02 +0900

    feat: サーバーコード実装

--- webrtc/server/signaling/src/index.ts
+++ webrtc/server/signaling/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello, world!');
+import WebSocket, { Server, WebSocket as WS } from 'ws';
+
+const server = new Server({ port: 3000 });
+
+server.on('connection', (socket: WS) => {
+    console.log('Client connected');
+
+    socket.on('message', (message: string) => {
+        // メッセージをすべてのクライアントにブロードキャスト
+        server.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN && client !== socket) {
+                client.send(message);
+            }
+        });
+    });
+
+    socket.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
