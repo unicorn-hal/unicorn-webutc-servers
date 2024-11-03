0c95cbec59a7cf47c34e8ec3ef889f983e782ef5
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-03 17:01:14 +0900

    feat: flutter web app仮実装

--- webrtc/app/flutter/lib/main.dart
+++ webrtc/app/flutter/lib/main.dart
@@ -1,125 +1,225 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter WebRTC Demo',
+        home: WebRTCSample(),
+        debugShowCheckedModeBanner: false);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class WebRTCSample extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _WebRTCSampleState createState() => _WebRTCSampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WebRTCSampleState extends State<WebRTCSample> {
+  late RTCPeerConnection _peerConnection;
+  late MediaStream _localStream;
+  late WebSocketChannel _channel;
+  final _remoteRenderer = RTCVideoRenderer();
+  final _localRenderer = RTCVideoRenderer();
+  List<String> _peers = []; // List to store available peers
+  String? _selectedPeer; // Selected peer
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _initRenderers();
+    _connectToSignalingServer();
+    _createPeerConnection().then((pc) {
+      _peerConnection = pc;
+      _startLocalStream();
     });
   }
 
   @override
+  void dispose() {
+    _localRenderer.dispose();
+    _remoteRenderer.dispose();
+    _peerConnection.close();
+    _channel.sink.close();
+    super.dispose();
+  }
+
+  Future<void> _initRenderers() async {
+    await _localRenderer.initialize();
+    await _remoteRenderer.initialize();
+  }
+
+  Future<void> _startLocalStream() async {
+    final Map<String, dynamic> mediaConstraints = {
+      'audio': true,
+      'video': {
+        'facingMode': 'user',
+      }
+    };
+
+    _localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+    _localRenderer.srcObject = _localStream;
+
+    _localStream.getTracks().forEach((track) {
+      _peerConnection.addTrack(track, _localStream);
+    });
+  }
+
+  Future<RTCPeerConnection> _createPeerConnection() async {
+    final Map<String, dynamic> configuration = {
+      'iceServers': [
+        {'urls': 'stun:localhost:3478'},
+      ],
+    };
+
+    final pc = await createPeerConnection(configuration);
+
+    pc.onIceCandidate = (RTCIceCandidate candidate) {
+      if (candidate != null) {
+        _sendSignalingMessage({
+          'type': 'candidate',
+          'candidate': candidate.toMap(),
+        });
+      }
+    };
+
+    pc.onTrack = (RTCTrackEvent event) {
+      if (event.track.kind == 'video') {
+        setState(() {
+          _remoteRenderer.srcObject = event.streams.first;
+        });
+      }
+    };
+
+    return pc;
+  }
+
+  void _connectToSignalingServer() {
+    _channel = WebSocketChannel.connect(Uri.parse('ws://localhost:3000'));
+
+    _channel.stream.listen((message) {
+      var data = json.decode(message);
+      switch (data['type']) {
+        case 'offer':
+          _handleOffer(data);
+          break;
+        case 'answer':
+          _handleAnswer(data);
+          break;
+        case 'candidate':
+          _handleCandidate(data);
+          break;
+        case 'peers':
+          setState(() {
+            _peers = List<String>.from(data['peers']);
+          });
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  void _sendSignalingMessage(Map<String, dynamic> message) {
+    _channel.sink.add(json.encode(message));
+  }
+
+  void _handleOffer(Map<String, dynamic> data) async {
+    await _peerConnection.setRemoteDescription(
+      RTCSessionDescription(data['sdp'], 'offer'),
+    );
+    RTCSessionDescription answer = await _peerConnection.createAnswer();
+    await _peerConnection.setLocalDescription(answer);
+
+    _sendSignalingMessage({
+      'type': 'answer',
+      'sdp': answer.sdp,
+    });
+  }
+
+  void _handleAnswer(Map<String, dynamic> data) async {
+    await _peerConnection.setRemoteDescription(
+      RTCSessionDescription(data['sdp'], 'answer'),
+    );
+  }
+
+  void _handleCandidate(Map<String, dynamic> data) async {
+    RTCIceCandidate candidate = RTCIceCandidate(
+      data['candidate']['candidate'],
+      data['candidate']['sdpMid'],
+      int.parse(data['candidate']['sdpMLineIndex'].toString()),
+    );
+    await _peerConnection.addCandidate(candidate);
+  }
+
+  Future<void> _createOffer() async {
+    if (_selectedPeer == null) return;
+
+    RTCSessionDescription offer = await _peerConnection.createOffer();
+    await _peerConnection.setLocalDescription(offer);
+
+    _sendSignalingMessage({
+      'type': 'offer',
+      'sdp': offer.sdp,
+      'target': _selectedPeer,
+    });
+  }
+
+  void _showPeerSelectionDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Select Peer'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: _peers.map((peer) {
+                return ListTile(
+                  title: Text(peer),
+                  onTap: () {
+                    setState(() {
+                      _selectedPeer = peer;
+                    });
+                    Navigator.of(context).pop();
+                    _createOffer();
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter WebRTC Demo'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: RTCVideoView(_localRenderer, mirror: true),
+          ),
+          Expanded(
+            child: RTCVideoView(_remoteRenderer),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _showPeerSelectionDialog,
+        tooltip: 'Start Call',
+        child: Icon(Icons.phone),
+      ),
     );
   }
 }
